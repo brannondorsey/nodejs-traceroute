22a672d62919a88814a66b541f19417f5eece6da
Brannon Dorsey <brannon@brannondorsey.com>
2016-09-02 12:45:04 -0500

    Add ASN to hop object in traceroute.js

--- traceroute.js
+++ traceroute.js
@@ -4,7 +4,7 @@ const Process = require('./process');
 
 class Traceroute extends Process {
     constructor() {
-        super('traceroute', ['-q', 1, '-n']);
+        super('traceroute', ['-q', 1, '-n', '-A']);
     }
 
     parseDestination(data) {
@@ -20,23 +20,25 @@ class Traceroute extends Process {
     }
 
     parseHop(hopData) {
-        const regex = /^\s*(\d+)\s+(?:([a-zA-Z0-9:.]+)\s+([0-9.]+\s+ms)|(\*))/;
+        const regex = /^\s*(\d+)\s+(?:([a-zA-Z0-9:.]+)\s\[(AS\d+|\*)\]\s+([0-9.]+\s+ms)|(\*))/;
         const parsedData = new RegExp(regex, '').exec(hopData);
 
         let result = null;
         if (parsedData !== null) {
-            if (parsedData[4] === undefined) {
+            if (parsedData[5] === undefined) {
                 result = {
                     hop: parseInt(parsedData[1], 10),
                     ip: parsedData[2],
-                    rtt1: parsedData[3]
+                    rtt1: parsedData[4],
+                    asn: parsedData[3] == '*' ? parsedData[3] : parsedData[3].substring(2)
                 };
             }
             else {
                 result = {
                     hop: parseInt(parsedData[1], 10),
-                    ip: parsedData[4],
-                    rtt1: parsedData[4]
+                    ip: parsedData[5],
+                    rtt1: parsedData[5],
+                    asn: '*'
                 };
             }
         }
